20bcc1f511fce51d1314468c2feeba0853bfc98d
michael isbell <prenezisbell@gmail.com>
2019-11-30 18:54:55 -0500

    Initial Commit

---
+++ RAC from Github/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RAC from Github
+//
+//  Created by michael isbell on 11/30/19.
+//  Copyright © 2019 Advanced Mobile Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
